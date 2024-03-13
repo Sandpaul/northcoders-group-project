cc688614449f97ada510c9cffcd62fd8ca283486
Sandpaul <paul_sandford@hotmail.co.uk>
2024-03-13 22:10:13 +0000

    add parquet_file_reader() functionality and test to make sure function retrieves a df from a parquet file in an s3 bucket.

--- src/utils/parquet_file_reader.py
+++ src/utils/parquet_file_reader.py
@@ -1,4 +1,36 @@
 """This module contains the definition for `parquet_file_reader()`."""
 
-def parquet_file_reader():
-    pass
+import io
+
+import boto3
+import pandas as pd
+
+def parquet_file_reader(file_path, bucket_name):
+    """A function to retrieve a data frame from a parquet file.
+
+    Args:
+        file_path (str): string of the file path to the required file.
+        e.g. `tablename/YYYY-MM-DD/HH.MM.SS.SSSSSSS`
+        
+        bucket_name (str): name of the s3 bucket where the required parquet file is stored.
+        e.g. `totesys-etl-ingestion-bucket-teamness-120224`
+
+    Returns:
+        df (data frame): the data frame from the read parquet file.
+        
+    Raises:
+    
+    
+    """
+    s3 = boto3.client("s3")
+    
+    response = s3.get_object(
+        Bucket=bucket_name,
+        Key=file_path
+    )
+    
+    file_contents = response["Body"].read()
+    content_in_bytes = io.BytesIO(file_contents)
+    df = pd.read_parquet(content_in_bytes)
+    
+    return df
--- test/test_utils/test_parquet_file_reader.py
+++ test/test_utils/test_parquet_file_reader.py
@@ -1,5 +1,6 @@
 """This module contains the test suite for `parquet_file_reader()`."""
 
+import io
 import os
 
 import boto3
@@ -30,42 +31,35 @@ def s3(aws_credentials):
 @pytest.fixture
 def example_data():
     """Create mock data."""
-    return {
-        "cars": [
+    mock_data = [
             {"id": 1, "make": "Ford", "model": "Mustang"},
             {"id": 2, "make": "Toyota", "model": "Yaris"},
             {"id": 3, "make": "Honda", "model": "Civic"},
             {"id": 4, "make": "BMW", "model": "X5"},
-        ],
-    }
+        ]
+    return pd.DataFrame.from_records(mock_data)
 
 
 @pytest.fixture
-def example_df(example_data):
-    """Create example dataframe."""
-    data_to_write = example_data["cars"]
-    return pd.DataFrame.from_records(data_to_write)
-
-
-@pytest.fixture
-def example_dict(example_df):
-    """Create example dict."""
-    return {
-        "timestamp": '2022-11-03 14:20:51.563',
-        "cars": example_df
-    }
-
-
-@pytest.fixture
-def bucket(s3, example_dict):
+def bucket(s3, example_data):
     """Create mock s3 bucket with parquet file."""
     s3.create_bucket(
         Bucket="totesys-etl-ingestion-bucket-teamness-120224",
         CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
     )
-    data_to_write = example_dict
+    data_to_write = pd.DataFrame.to_parquet(example_data)
     s3.put_object(
         Body=data_to_write,
         Bucket="totesys-etl-ingestion-bucket-teamness-120224",
         Key="cars/2022-11-03/14:20:51.563/.parquet",
     )
+    
+
+@pytest.mark.describe("parquet_file_reader()")
+@pytest.mark.it("should retrieve data frame from file")
+def test_retrieve_df_from_file(s3, example_data, bucket):
+    """parquet_file_reader() should use passed filepath to retrieve file from s3 and return dataframe."""
+    file_path = "cars/2022-11-03/14:20:51.563/.parquet"
+    bucket_name = "totesys-etl-ingestion-bucket-teamness-120224"
+    result = parquet_file_reader(file_path, bucket_name)
+    assert type(result).__name__ == "DataFrame"
