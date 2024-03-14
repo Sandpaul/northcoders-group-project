5408701145deda5c8f1e6d264f9fbde7f84df5be
Sandpaul <paul_sandford@hotmail.co.uk>
2024-03-14 10:08:45 +0000

    Refactor transform/dim_currency() and test suite to transform from parquet file.

--- test/test_transform/test_dim_currency.py
+++ test/test_transform/test_dim_currency.py
@@ -1,9 +1,14 @@
-from src.transform.read_ingestion_file_data import read_ingestion_file_data
-from src.transform.dim_currency import transform_currency
 import os
+import io
+import json
+
 import boto3
 from moto import mock_aws
 import pytest
+import pandas as pd
+
+from src.utils.parquet_file_reader import parquet_file_reader
+from src.transform.dim_currency import dim_currency
 
 
 @pytest.fixture(scope="function")
@@ -24,101 +29,84 @@ def s3(aws_credentials):
 
 
 @pytest.fixture
-def example_data():
-    """Create mock data."""
-    return {
-        "timestamp": "2024-02-22 11:35:20.078376",
-        "currency": [
-            {
-                "created_at": "2022-11-03 14:20:49.962000",
-                "currency_code": "GBP",
-                "currency_id": 1,
-                "last_updated": "2022-11-03 14:20:49.962000",
-            },
-            {
-                "created_at": "2022-11-03 14:20:49.962000",
-                "currency_code": "USD",
-                "currency_id": 2,
-                "last_updated": "2022-11-03 14:20:49.962000",
-            },
-            {
-                "created_at": "2022-11-03 14:20:49.962000",
-                "currency_code": "EUR",
-                "currency_id": 3,
-                "last_updated": "2022-11-03 14:20:49.962000",
-            },
-        ],
-    }
-
-
-@pytest.fixture
-def bucket(s3, example_data):
+def bucket(s3):
     """Create mock s3 bucket."""
     s3.create_bucket(
         Bucket="totesys-etl-ingestion-bucket-teamness-120224",
         CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
     )
     with open("test/test_transform/test_data/test_currency_data.json") as f:
-        data_to_write = f.read()
+        data = f.read()
+        json_data = json.loads(data)
+        df = pd.DataFrame.from_records(json_data["currency"])
+        parquet = pd.DataFrame.to_parquet(df)
         s3.put_object(
-            Body=data_to_write,
+            Body=parquet,
             Bucket="totesys-etl-ingestion-bucket-teamness-120224",
-            Key="currency/2024-02-22/11:35:20.078375.json",
+            Key="currency/2024-02-22/11:35:20.078376.parquet",
         )
-    with open("test/test_transform/test_data/test_currency_data2.json") as f:
-        data_to_write = f.read()
-        s3.put_object(
-            Body=data_to_write,
-            Bucket="totesys-etl-ingestion-bucket-teamness-120224",
-            Key="currency/2024-02-22/11:35:20.078376.json",
-        )
+        
+
+@pytest.fixture
+def control_df():
+    """Sets up a control data frame."""
+    with open("test/test_transform/test_data/test_currency_data.json") as f:
+        data = f.read()
+        json_data = json.loads(data)
+        df = pd.DataFrame.from_records(json_data["currency"])
+        return df
 
 
-@pytest.mark.describe("transform_currency()")
-@pytest.mark.it("should return dictionary with a dataframe")
-def test_transform_currency_returns_df(s3, bucket, example_data):
-    """should return a dictionary with a dataframe"""
-    file_path = "currency/2024-02-22/11:35:20.078375.json"
-    test_data = read_ingestion_file_data(file_path)
-    result = transform_currency(test_data)
-    assert type(result["currency"]).__name__ == "DataFrame"
+@pytest.fixture
+def file_path():
+    """Mock filepath."""
+    return "currency/2024-02-22/11:35:20.078376.parquet"
 
 
-@pytest.mark.describe("transform_currency()")
-@pytest.mark.it("dataframe should contain correct number of keys")  # noqa
-def test_transform_design_returns_correct_created(s3, bucket, example_data):
-    file_path = "currency/2024-02-22/11:35:20.078375.json"
-    test_data = read_ingestion_file_data(file_path)
-    result = transform_currency(test_data)
-    assert set(result["currency"].keys()) == set(
+@pytest.fixture
+def bucket_name():
+    """Mock bucket name."""
+    return "totesys-etl-ingestion-bucket-teamness-120224"
+
+
+@pytest.mark.describe("dim_currency()")
+@pytest.mark.it("should return a dataframe")
+def test_dim_currency_returns_df(s3, bucket, file_path, bucket_name):
+    """should return a new dataframe"""
+    test_data = parquet_file_reader(file_path, bucket_name)
+    result = dim_currency(test_data)
+    assert type(result).__name__ == "DataFrame"
+
+
+@pytest.mark.describe("dim_currency()")
+@pytest.mark.it("returned dataframe should contain correct keys")  # noqa
+def test_dim_currency_returns_correct_num_keys(s3, bucket, file_path, bucket_name):
+    """dim_currency() should return the correct keys."""
+    test_data = parquet_file_reader(file_path, bucket_name)
+    result = dim_currency(test_data)
+    assert set(result.keys()) == set(
         [
-            "currency_record_id",
-            "currency_code",
             "currency_id",
+            "currency_code",
             "currency_name",
-            "last_updated_date",
-            "last_updated_time",
         ]
-    )  # noqa
+    )
 
 
-@pytest.mark.describe("transform_currency()")
+@pytest.mark.describe("dim_currency()")
 @pytest.mark.it("dataframe should contain correct currency_name")  # noqa
-def test_transform_currency_returns_correct_currency_name(s3, bucket, example_data):
+def test_dim_currency_returns_correct_currency_name(s3, bucket, file_path, bucket_name):
     """should return the correct currency_name"""
-    file_path = "currency/2024-02-22/11:35:20.078376.json"
-    test_data = read_ingestion_file_data(file_path)
-    result = transform_currency(test_data)
-    assert result["currency"].get("currency_name").get(0) == "British Pound"
-    assert result["currency"].get("currency_name").get(1) == "US dollar"
+    test_data = parquet_file_reader(file_path, bucket_name)
+    result = dim_currency(test_data)
+    assert result.get("currency_name").get(0) == "British Pound"
+    
 
-
-@pytest.mark.describe("transform_currency()")
-@pytest.mark.it("should work when passed currency list with more than one dict")  # noqa
-def test_transform_currency_works_on_multiple_dicts(s3, bucket, example_data):
-    file_path = "currency/2024-02-22/11:35:20.078376.json"
-    test_data = read_ingestion_file_data(file_path)
-    result = transform_currency(test_data)
-
-    assert result["currency"].get("currency_name").get(0) == "British Pound"
-    assert result["currency"].get("currency_name").get(1) == "US dollar"
+@pytest.mark.describe("dim_currency()")
+@pytest.mark.it("should not mutate original dataframe")  # noqa
+def test_dim_currency_works_on_multiple_rows(s3, bucket, file_path, bucket_name, control_df):
+    """dim_currency() should not mutate the passed dataframe."""
+    test_data = parquet_file_reader(file_path, bucket_name)
+    result = dim_currency(test_data)
+    assert test_data.equals(control_df) is True
+    assert test_data.equals(result) is False
