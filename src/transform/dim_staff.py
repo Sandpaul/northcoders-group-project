3d0c5557cab7ed5b2a78b009bf8ea1c85e3a963f
Sandpaul <paul_sandford@hotmail.co.uk>
2024-03-14 19:47:22 +0000

    refactor dim_staff() and test suite to use data frames rather than json.

--- src/transform/dim_staff.py
+++ src/transform/dim_staff.py
@@ -1,78 +1,124 @@
-"""This module contains the definition for transform_staff()."""
+# """This module contains the definition for transform_staff()."""
 
-import json
+# import json
+# import pandas as pd
+# import boto3
+
+
+# def transform_staff(staff_data):
+#     """Function to transform data stored in ingestion bucket that was extracted
+#     from staff table in totesys.
+
+#     Args:
+#         staff_data (dict): dict of data from staff file from
+#         ingestion bucket
+
+#     Returns:
+#         staff_data_copy (dict): copy of staff_data with data dict
+#         replaced by dataframe
+#     """
+#     s3 = boto3.client("s3")
+
+#     response = s3.list_objects(
+#         Bucket="totesys-etl-ingestion-bucket-teamness-120224",
+#         Prefix="department/",
+#         Delimiter="/",
+#     )  # noqa
+
+#     subfolders = [
+#         common_prefix["Prefix"] for common_prefix in response.get("CommonPrefixes", [])
+#     ]  # noqa
+
+#     department_merged_df = pd.DataFrame()
+
+#     files_list = []
+
+#     for folder in subfolders:
+#         folder_response = s3.list_objects(
+#             Bucket="totesys-etl-ingestion-bucket-teamness-120224", Prefix=folder
+#         )  # noqa
+#         folder_objects = folder_response.get("Contents", [])
+
+#         department_file = [item["Key"] for item in folder_objects]
+
+#         files_list.append(department_file[0])
+
+#         for file in files_list:
+#             file_content = (
+#                 s3.get_object(
+#                     Bucket="totesys-etl-ingestion-bucket-teamness-120224", Key=file
+#                 )["Body"]
+#                 .read()
+#                 .decode("utf-8")
+#             )  # noqa
+
+#         parsed_data = json.loads(file_content)
+
+#         department_data = parsed_data["department"]
+
+#         file_df = pd.DataFrame.from_records(department_data)
+
+#         department_merged_df = pd.concat(
+#             [department_merged_df, file_df], ignore_index=True
+#         )
+
+#     staff_data_copy = staff_data.copy()
+
+#     staff_rows = staff_data_copy["staff"]
+
+#     staff_df = pd.DataFrame.from_records(staff_rows)
+
+#     dim_staff_df = pd.merge(
+#         staff_df,
+#         department_merged_df,
+#         left_on="department_id",
+#         right_on="department_id",
+#     )
+
+#     dim_staff_df = dim_staff_df[
+#         [
+#             "staff_id",
+#             "first_name",
+#             "last_name",
+#             "department_name",
+#             "location",
+#             "email_address",
+#         ]
+#     ]
+
+#     dim_staff_df["last_updated_date"] = "1970-01-01"
+#     dim_staff_df["last_updated_time"] = "00:00"
+
+#     dim_staff_df["last_updated_date"] = pd.to_datetime(
+#         dim_staff_df["last_updated_date"], format="%Y-%m-%d"
+#     ).dt.date
+#     dim_staff_df["last_updated_time"] = pd.to_datetime(
+#         dim_staff_df["last_updated_time"], format="%H:%M"
+#     ).dt.time
+
+#     staff_data_copy["staff"] = dim_staff_df
+
+#     return staff_data_copy
+
 import pandas as pd
-import boto3
+
+from src.utils.get_archived_table_data import get_archived_table_data
+from src.utils.get_bucket_name import get_bucket_name
 
 
-def transform_staff(staff_data):
-    """Function to transform data stored in ingestion bucket that was extracted
-    from staff table in totesys.
+def dim_staff(staff_data):
 
-    Args:
-        staff_data (dict): dict of data from staff file from
-        ingestion bucket
+    bucket_name = get_bucket_name("ingestion")
 
-    Returns:
-        staff_data_copy (dict): copy of staff_data with data dict
-        replaced by dataframe
-    """
-    s3 = boto3.client("s3")
+    department_df = get_archived_table_data(
+        table_name="department",
+        bucket_name=bucket_name,
+    )
 
-    response = s3.list_objects(
-        Bucket="totesys-etl-ingestion-bucket-teamness-120224",
-        Prefix="department/",
-        Delimiter="/",
-    )  # noqa
-
-    subfolders = [
-        common_prefix["Prefix"] for common_prefix in response.get("CommonPrefixes", [])
-    ]  # noqa
-
-    department_merged_df = pd.DataFrame()
-
-    files_list = []
-
-    for folder in subfolders:
-        folder_response = s3.list_objects(
-            Bucket="totesys-etl-ingestion-bucket-teamness-120224", Prefix=folder
-        )  # noqa
-        folder_objects = folder_response.get("Contents", [])
-
-        department_file = [item["Key"] for item in folder_objects]
-
-        files_list.append(department_file[0])
-
-        for file in files_list:
-            file_content = (
-                s3.get_object(
-                    Bucket="totesys-etl-ingestion-bucket-teamness-120224", Key=file
-                )["Body"]
-                .read()
-                .decode("utf-8")
-            )  # noqa
-
-        parsed_data = json.loads(file_content)
-
-        department_data = parsed_data["department"]
-
-        file_df = pd.DataFrame.from_records(department_data)
-
-        department_merged_df = pd.concat(
-            [department_merged_df, file_df], ignore_index=True
-        )
-
-    staff_data_copy = staff_data.copy()
-
-    staff_rows = staff_data_copy["staff"]
-
-    staff_df = pd.DataFrame.from_records(staff_rows)
+    staff_df = staff_data.copy(deep=True)
 
     dim_staff_df = pd.merge(
-        staff_df,
-        department_merged_df,
-        left_on="department_id",
-        right_on="department_id",
+        staff_df, department_df, left_on="department_id", right_on="department_id"
     )
 
     dim_staff_df = dim_staff_df[
@@ -86,16 +132,4 @@ def transform_staff(staff_data):
         ]
     ]
 
-    dim_staff_df["last_updated_date"] = "1970-01-01"
-    dim_staff_df["last_updated_time"] = "00:00"
-
-    dim_staff_df["last_updated_date"] = pd.to_datetime(
-        dim_staff_df["last_updated_date"], format="%Y-%m-%d"
-    ).dt.date
-    dim_staff_df["last_updated_time"] = pd.to_datetime(
-        dim_staff_df["last_updated_time"], format="%H:%M"
-    ).dt.time
-
-    staff_data_copy["staff"] = dim_staff_df
-
-    return staff_data_copy
+    return dim_staff_df
--- test/test_transform/test_dim_staff.py
+++ test/test_transform/test_dim_staff.py
@@ -1,11 +1,14 @@
 """This module contains the test suite for transform_staff()."""
 
 import os
-from moto import mock_aws
+import json
+
 import boto3
+from moto import mock_aws
+import pandas as pd
 import pytest
-from src.transform.dim_staff import transform_staff
-from src.transform.read_ingestion_file_data import read_ingestion_file_data
+
+from src.transform.dim_staff import dim_staff
 
 
 @pytest.fixture(scope="function")
@@ -26,110 +29,115 @@ def s3(aws_credentials):
 
 
 @pytest.fixture
-def bucket(s3):
+def department_df_1():
+    """Sets up a test data frame."""
+    with open("test/test_transform/test_data/test_department_data1.json") as f:
+        data = f.read()
+        json_data = json.loads(data)
+        df = pd.DataFrame.from_records(json_data["department"])
+        return df
+
+
+@pytest.fixture
+def department_df_2():
+    """Sets up a test data frame."""
+    with open("test/test_transform/test_data/test_department_data2.json") as f:
+        data = f.read()
+        json_data = json.loads(data)
+        df = pd.DataFrame.from_records(json_data["department"])
+        return df
+
+
+@pytest.fixture
+def staff_df():
+    """Sets up a test data frame."""
+    with open("test/test_transform/test_data/test_staff_data.json") as f:
+        data = f.read()
+        json_data = json.loads(data)
+        df = pd.DataFrame.from_records(json_data["staff"])
+        return df
+
+
+@pytest.fixture
+def control_df():
+    """Sets up a control data frame."""
+    with open("test/test_transform/test_data/test_staff_data.json") as f:
+        data = f.read()
+        json_data = json.loads(data)
+        df = pd.DataFrame.from_records(json_data["staff"])
+        return df
+
+
+@pytest.fixture
+def bucket(s3, department_df_1, department_df_2):
     """Create mock s3 bucket."""
     s3.create_bucket(
         Bucket="totesys-etl-ingestion-bucket-teamness-120224",
         CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
     )
-    with open("test/test_transform/test_data/test_staff_data.json") as f:  # noqa
-        data_to_write = f.read()
-        s3.put_object(
-            Body=data_to_write,
-            Bucket="totesys-etl-ingestion-bucket-teamness-120224",
-            Key="staff/2022-11-03/14:20:51.563.json",
-        )
-    with open("test/test_transform/test_data/test_department_data1.json") as f:
-        data_to_write = f.read()
-        s3.put_object(
-            Body=data_to_write,
-            Bucket="totesys-etl-ingestion-bucket-teamness-120224",
-            Key="department/2022-11-03/14:20:51.563.json",
-        )
-    with open("test/test_transform/test_data/test_department_data2.json") as f:
-        data_to_write = f.read()
-        s3.put_object(
-            Body=data_to_write,
-            Bucket="totesys-etl-ingestion-bucket-teamness-120224",
-            Key="department/2022-11-04/14:20:51.563.json",
-        )
+    data_to_write_1 = pd.DataFrame.to_parquet(department_df_1)
+    s3.put_object(
+        Body=data_to_write_1,
+        Bucket="totesys-etl-ingestion-bucket-teamness-120224",
+        Key="department/2022-11-03/14:20:51.563.json",
+    )
+    data_to_write_2 = pd.DataFrame.to_parquet(department_df_2)
+    s3.put_object(
+        Body=data_to_write_2,
+        Bucket="totesys-etl-ingestion-bucket-teamness-120224",
+        Key="department/2022-11-04/14:20:51.563.json",
+    )
 
 
-@pytest.mark.describe("transform_staff")
-@pytest.mark.it("should return a dictionary with dataframe")
-def test_transform_staff_returns_a_dictionary(s3, bucket):
+@pytest.mark.describe("dim_staff()")
+@pytest.mark.it("should return a dataframe")
+def test_dim_staff_returns_a_dataframe(bucket, staff_df):
     """Should return a dictionary with dataframe"""
-    staff_file_path = "staff/2022-11-03/14:20:51.563.json"
-    staff_test_data = read_ingestion_file_data(staff_file_path)
-    result = transform_staff(staff_test_data)
-    assert isinstance(result, dict)
+    result = dim_staff(staff_df)
+    assert type(result).__name__ == "DataFrame"
 
 
-@pytest.mark.describe("transform_staff")
-@pytest.mark.it("should return a dataframe in the staff key of the dictionary")  # noqa
-def test_function_returns_a_dataframe_in_the_dictionary(s3, bucket):
-    """Returned staff key should be a dataframe"""
-    staff_file_path = "staff/2022-11-03/14:20:51.563.json"
-    staff_test_data = read_ingestion_file_data(staff_file_path)
-    result = transform_staff(staff_test_data)
-    assert type(result["staff"]).__name__ == "DataFrame"
-
-
-@pytest.mark.describe("transform_staff")
+@pytest.mark.describe("dim_staff()")
 @pytest.mark.it("check if the dataframe has the required column names")
-def test_function_returns_the_correct_columns(s3, bucket):
+def test_function_returns_the_correct_columns(bucket, staff_df):
     """Returned dataframe has the required column names returned"""
-    staff_file_path = "staff/2022-11-03/14:20:51.563.json"
-    staff_test_data = read_ingestion_file_data(staff_file_path)
-    result = transform_staff(staff_test_data)
+    result = dim_staff(staff_df)
     expected = [
-        "staff_record_id",
         "staff_id",
         "first_name",
         "last_name",
         "department_name",
         "location",
         "email_address",
-        "last_updated_date",
-        "last_updated_time",
     ]
-    assert list(result["staff"].columns) == expected
+    assert list(result.columns) == expected
 
 
-@pytest.mark.describe("transform_staff")
+@pytest.mark.describe("dim_staff()")
 @pytest.mark.it("does not have the unwanted columns after merging")
-def test_function_deletes_unwanted_columns(s3, bucket):
+def test_function_deletes_unwanted_columns(bucket, staff_df):
     """Returned dataframe has the required column names returned"""
-    staff_file_path = "staff/2022-11-03/14:20:51.563.json"
-    staff_test_data = read_ingestion_file_data(staff_file_path)
-    result = transform_staff(staff_test_data)
-    column_headers = list(result["staff"].columns)
-    for column in column_headers:
-        assert "department_id" != column
-        assert "address_id" != column
-        assert "created_at" != column
-        assert "last_updated" != column
-        assert "manager" != column
+    result = dim_staff(staff_df)
+    assert "created_at" not in result.columns
+    assert "last_updated" not in result.columns
+    assert "department_id" not in result.columns
+    assert "manager" not in result.columns
 
 
-@pytest.mark.describe("transform_staff")
-@pytest.mark.it("returns the correct timestamp")
-def test_function_returns_right_timestamp(s3, bucket):
-    """Returned dataframe has the required column names returned"""
-    staff_file_path = "staff/2022-11-03/14:20:51.563.json"
-    staff_test_data = read_ingestion_file_data(staff_file_path)
-    result = transform_staff(staff_test_data)["timestamp"]  # noqa
-    expected = "2024-02-22 17:25:21.054448"
-    assert result == expected
-
-
-@pytest.mark.describe("transform_staff")
+@pytest.mark.describe("dim_staff()")
 @pytest.mark.it("should join the staff and address data correctly")
-def test_function_joins_correctly(s3, bucket):
-    """transform_staff should complete the correct joins succesfully"""
-    staff_file_path = "staff/2022-11-03/14:20:51.563.json"
-    staff_test_data = read_ingestion_file_data(staff_file_path)
-    result = transform_staff(staff_test_data)
-    assert result["staff"].get("department_name").get(0) == "Purchasing"
-    assert result["staff"].get("department_name").get(1) == "Facilities"
-    assert result["staff"].get("department_name").get(2) == "Production"
+def test_function_joins_correctly(bucket, staff_df):
+    """dim_staff should complete the correct joins succesfully"""
+    result = dim_staff(staff_df)
+    assert result.get("department_name").get(0) == "Purchasing"
+    assert result.get("department_name").get(1) == "Facilities"
+    assert result.get("department_name").get(2) == "Production"
+
+
+@pytest.mark.describe("dim_staff()")
+@pytest.mark.it("should not mutate passed data frames")
+def test_does_not_mutate_arg(s3, bucket, staff_df, control_df):
+    """dim_staff() should not mutate passed data frame."""
+    result = dim_staff(staff_df)
+    assert staff_df.equals(control_df) is True
+    assert result.equals(control_df) is False
