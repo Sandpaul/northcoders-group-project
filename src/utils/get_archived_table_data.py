9c4e14bae152d2fdd8450c6d48f56703b1abab90
Sandpaul <paul_sandford@hotmail.co.uk>
2024-03-14 16:27:12 +0000

    create get_archived_tabled_data() and associated test suite.

---
+++ src/utils/get_archived_table_data.py
@@ -0,0 +1,50 @@
+"""This module contains the definition for `get_archived_table_data()`."""
+
+import pandas as pd
+import boto3
+
+from src.utils.parquet_file_reader import parquet_file_reader
+
+def get_archived_table_data(table_name, bucket_name):
+    """A function to retrieve all file data from specified table in an s3 bucket and return it as a joined data frame.
+
+    Args:
+        table_name (str): string of the table name that you wish to retrieve data for.
+        bucket_name (str): name of the s3 bucket where data is stored
+    
+    Returns:
+        df (data frame): a data frame with all of the data from the passed table name combined.
+    """
+    
+    s3 = boto3.client("s3")
+
+    response = s3.list_objects(
+        Bucket=bucket_name,
+        Prefix=f"{table_name}/",
+        Delimiter="/",
+    )
+
+    subfolders = [
+        common_prefix["Prefix"] for common_prefix in response.get("CommonPrefixes", [])
+    ]
+
+    merged_df = pd.DataFrame()
+
+    files_list = []
+
+    for folder in subfolders:
+        folder_response = s3.list_objects(
+            Bucket=bucket_name, Prefix=folder,
+        )
+        folder_objects = folder_response.get("Contents")
+
+        file = [item["Key"] for item in folder_objects]
+
+        files_list.append(file[0])
+
+    for file in files_list:
+        file_df = parquet_file_reader(file, bucket_name)
+
+        merged_df = pd.concat([merged_df, file_df], ignore_index=True)
+    
+    return merged_df
---
+++ test/test_utils/test_get_archived_table_data.py
@@ -0,0 +1,94 @@
+"""This module contains the test suite for `get_archived_table_data()`."""
+
+import json
+import os
+
+import boto3
+from moto import mock_aws
+import pandas as pd
+import pytest
+
+from src.utils.get_archived_table_data import get_archived_table_data
+
+
+@pytest.fixture(scope="function")
+def aws_credentials():
+    """Mocked AWS Credentials for moto"""
+    os.environ["AWS_ACCESS_KEY_ID"] = "testing"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
+    os.environ["AWS_SECURITY_TOKEN"] = "testing"
+    os.environ["AWS_SESSION_TOKEN"] = "testing"
+    os.environ["AWS_DEFAULT_REGION"] = "eu-west-2"
+
+
+@pytest.fixture(scope="function")
+def s3(aws_credentials):
+    """Create mock s3 client."""
+    with mock_aws():
+        yield boto3.client("s3", region_name="eu-west-2")
+
+
+@pytest.fixture
+def test_df_1():
+    """Sets up a test data frame."""
+    with open("test/test_transform/test_data/test_department_data1.json") as f:
+        data = f.read()
+        json_data = json.loads(data)
+        df = pd.DataFrame.from_records(json_data["department"])
+        return df
+
+
+@pytest.fixture
+def test_df_2():
+    """Sets up a test data frame."""
+    with open("test/test_transform/test_data/test_department_data2.json") as f:
+        data = f.read()
+        json_data = json.loads(data)
+        df = pd.DataFrame.from_records(json_data["department"])
+        return df
+
+
+@pytest.fixture
+def bucket(s3, test_df_1, test_df_2):
+    """Create mock s3 bucket with parquet files."""
+    s3.create_bucket(
+        Bucket="test_bucket",
+        CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
+    )
+    data_to_write_1 = pd.DataFrame.to_parquet(test_df_1)
+    s3.put_object(
+        Body=data_to_write_1,
+        Bucket="test_bucket",
+        Key="department/2022-11-03/14:20:51.563/.parquet",
+    )
+    data_to_write_2 = pd.DataFrame.to_parquet(test_df_2)
+    s3.put_object(
+        Body=data_to_write_2,
+        Bucket="test_bucket",
+        Key="department/2022-11-04/14:20:51.563/.parquet",
+    )
+
+
+@pytest.mark.describe("get_archived_table_data()")
+@pytest.mark.it("should return a dataframe")
+def test_returns_df(bucket):
+    """get_archived_table_data() should return a dataframe."""
+    result = get_archived_table_data("department", "test_bucket")
+    assert type(result).__name__ == "DataFrame"
+    
+
+@pytest.mark.describe("get_archived_table_data()")
+@pytest.mark.it("should return a dataframe with correct number of rows")
+def test_returns_correct_row_num(bucket, test_df_1, test_df_2):
+    """get_archived_table_data() should return correct number of rows."""
+    result = get_archived_table_data("department", "test_bucket")
+    assert len(result) == len(test_df_1) + len(test_df_2)
+
+
+@pytest.mark.describe("get_archived_table_data()")
+@pytest.mark.it("should return a dataframe with correct data")
+def test_returns_correct_data(bucket, test_df_1, test_df_2):
+    """get_archived_table_data() should return correct data."""
+    merged_df = pd.concat([test_df_1, test_df_2], ignore_index=True)
+    result = get_archived_table_data("department", "test_bucket")
+    assert result.equals(merged_df)
