85ddf44b78db9c45d1182aa4d5ba70c88bdcbdab
Sandpaul <paul_sandford@hotmail.co.uk>
2024-03-14 11:47:18 +0000

    add test to ensure passed df is not mutated.

--- src/transform/drop_created_and_updated.py
+++ src/transform/drop_created_and_updated.py
@@ -1,11 +1,12 @@
 """This module contains the definition for `drop_created_and_updated()`."""
 
+
 def drop_created_and_updated(data_frame):
     """A function to drop the `created_at` and `last_updated` columns from a data frame.
 
     Args:
         data_frame (data frame): the data frame from which created_at and last_updated columns are to be dropped.
-    
+
     Returns:
         df (data frame): the new data frame with created_at and last_updated columns removed.
     """
--- test/test_transform/test_drop_created_and_updated.py
+++ test/test_transform/test_drop_created_and_updated.py
@@ -7,10 +7,25 @@ import pytest
 
 from src.transform.drop_created_and_updated import drop_created_and_updated
 
+
 @pytest.fixture
 def test_data_frame():
     """Sets up a test data frame."""
-    with open("test/test_transform/test_data/test_drop_created_and_updated_data.json") as f:
+    with open(
+        "test/test_transform/test_data/test_drop_created_and_updated_data.json"
+    ) as f:
+        data = f.read()
+        json_data = json.loads(data)
+        df = pd.DataFrame.from_records(json_data["department"])
+        return df
+
+
+@pytest.fixture
+def control_data_frame():
+    """Sets up a control data frame."""
+    with open(
+        "test/test_transform/test_data/test_drop_created_and_updated_data.json"
+    ) as f:
         data = f.read()
         json_data = json.loads(data)
         df = pd.DataFrame.from_records(json_data["department"])
@@ -18,11 +33,20 @@ def test_data_frame():
 
 
 @pytest.mark.describe("drop_created_and_updated()")
-@pytest.mark.it("should return a new data frame with create_at and last_updated removed")
+@pytest.mark.it(
+    "should return a new data frame with created_at and last_updated removed"
+)
 def test_drop_created_and_updated(test_data_frame):
-    """drop_created_and_updated() should remove created_at and last_updated columns from passed data frame.
-    """
+    """drop_created_and_updated() should remove created_at and last_updated columns from passed data frame."""
     result = drop_created_and_updated(test_data_frame)
     assert "created_at" not in result.columns
     assert "last_updated" not in result.columns
 
+
+@pytest.mark.describe("drop_created_and_updated()")
+@pytest.mark.it("should not mutate passed data frame")
+def test_does_not_mutate_passed_df(test_data_frame, control_data_frame):
+    """drop_created_and_updated() should not mutate the passed data frame."""
+    result = drop_created_and_updated(test_data_frame)
+    assert test_data_frame.equals(control_data_frame)
+    assert result.equals(control_data_frame) is False
