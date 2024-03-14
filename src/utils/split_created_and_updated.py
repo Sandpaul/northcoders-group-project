16b55eec3b4effbaa65e984a9473b4df34c68177
Sandpaul <paul_sandford@hotmail.co.uk>
2024-03-14 13:15:56 +0000

    finish testing for split_created_and_updated

--- src/utils/split_created_and_updated.py
+++ src/utils/split_created_and_updated.py
@@ -2,6 +2,7 @@
 
 import pandas as pd
 
+
 def split_created_and_updated(data_frame):
     """A function to split the `created_at` column into `created_date` and `created_time` and `last_updated` into `last_updated_date` and `last_updated_time`.
 
@@ -13,16 +14,17 @@ def split_created_and_updated(data_frame):
     """
 
     df = data_frame.copy(deep=True)
-    
+
     df["created_at"] = pd.to_datetime(df["created_at"], format="%Y-%m-%d %H:%M:%S.%f")
     df["created_date"] = df["created_at"].dt.date
     df["created_time"] = df["created_at"].dt.time
     df.drop("created_at", axis=1, inplace=True)
-    
-    # df["last_updated"] = pd.to_datetime(format="%Y-%m-%d %H:%M:%S.%f")
-    
-    
 
-    # df.drop("last_updated", axis=1, inplace=True)
+    df["last_updated"] = pd.to_datetime(
+        df["last_updated"], format="%Y-%m-%d %H:%M:%S.%f"
+    )
+    df["last_updated_date"] = df["last_updated"].dt.date
+    df["last_updated_time"] = df["last_updated"].dt.time
+    df.drop("last_updated", axis=1, inplace=True)
 
     return df
--- test/test_utils/test_split_created_and_updated.py
+++ test/test_utils/test_split_created_and_updated.py
@@ -1,4 +1,5 @@
 """This module contains the definition for `split_created_and_updated()`."""
+
 import datetime
 import json
 
@@ -11,9 +12,7 @@ from src.utils.split_created_and_updated import split_created_and_updated
 @pytest.fixture
 def test_data_frame():
     """Sets up a test data frame."""
-    with open(
-        "test/test_transform/test_data/test_sales_order_data2.json"
-    ) as f:
+    with open("test/test_transform/test_data/test_sales_order_data2.json") as f:
         data = f.read()
         json_data = json.loads(data)
         df = pd.DataFrame.from_records(json_data["sales_order"])
@@ -23,9 +22,7 @@ def test_data_frame():
 @pytest.fixture
 def control_data_frame():
     """Sets up a control data frame."""
-    with open(
-        "test/test_transform/test_data/test_sales_order_data2.json"
-    ) as f:
+    with open("test/test_transform/test_data/test_sales_order_data2.json") as f:
         data = f.read()
         json_data = json.loads(data)
         df = pd.DataFrame.from_records(json_data["sales_order"])
@@ -38,15 +35,24 @@ def test_splits_created_at(test_data_frame):
     """split_created_and_updated() should split created_at column into created_date and created_time columns."""
     result = split_created_and_updated(test_data_frame)
     assert "created_date" in result.columns
+    assert "created_time" in result.columns
 
 
 @pytest.mark.describe("split_created_and_updated()")
-@pytest.mark.it("returns correct date and time data in created_date and created_time columns")
+@pytest.mark.it(
+    "returns correct date and time data in created_date and created_time columns"
+)
 def test_splits_created_at_correctly(test_data_frame):
     """split_created_and_updated() populate created_date and created_time columns with correct data."""
     result = split_created_and_updated(test_data_frame)
-    expected_dates = [datetime.date(2024, 2, 20), datetime.date(2024, 2, 19)]
-    expected_times = [datetime.time(15, 7, 9, 880000),datetime.time(15, 7, 9, 880000),]
+    expected_dates = [
+        datetime.date(2024, 2, 20),
+        datetime.date(2024, 2, 19),
+    ]
+    expected_times = [
+        datetime.time(15, 7, 9, 880000),
+        datetime.time(15, 7, 9, 880000),
+    ]
     dates = result["created_date"].tolist()
     times = result["created_time"].tolist()
     assert dates == expected_dates
@@ -59,3 +65,50 @@ def test_deletes_created_at(test_data_frame):
     """split_created_and_updated() should delete the created_at column"""
     result = split_created_and_updated(test_data_frame)
     assert "created_at" not in result.columns
+
+
+@pytest.mark.describe("split_created_and_updated()")
+@pytest.mark.it("splits last_updated into last_updated date and time")
+def test_splits_last_updated(test_data_frame):
+    """split_created_and_updated() should split last_updated column into last_updated_date and last_updated_times columns."""
+    result = split_created_and_updated(test_data_frame)
+    assert "last_updated_date" in result.columns
+    assert "last_updated_time" in result.columns
+
+
+@pytest.mark.describe("split_created_and_updated()")
+@pytest.mark.it(
+    "returns correct date and time data in last_updated_date and last_updated_times columns"
+)
+def test_splits_last_updated_correctly(test_data_frame):
+    """split_created_and_updated() populate last_updated_date and last_updated_times columns with correct data."""
+    result = split_created_and_updated(test_data_frame)
+    expected_dates = [
+        datetime.date(2024, 2, 20),
+        datetime.date(2024, 2, 19),
+    ]
+    expected_times = [
+        datetime.time(15, 7, 9, 880000),
+        datetime.time(15, 7, 9, 880005),
+    ]
+    dates = result["last_updated_date"].tolist()
+    times = result["last_updated_time"].tolist()
+    assert dates == expected_dates
+    assert times == expected_times
+
+
+@pytest.mark.describe("split_created_and_updated()")
+@pytest.mark.it("deletes last_updated column")
+def test_deletes_last_updated(test_data_frame):
+    """split_created_and_updated() should delete the last_updated column"""
+    result = split_created_and_updated(test_data_frame)
+    assert "last_updated" not in result.columns
+
+
+@pytest.mark.describe("split_created_and_updated()")
+@pytest.mark.it("should not mutate passed data frame")
+def test_does_not_mutate_arg(test_data_frame, control_data_frame):
+    """split_created_and_updated() should not mutate the passed dataframe."""
+    result = split_created_and_updated(test_data_frame)
+    assert test_data_frame.equals(control_data_frame)
+    assert result.equals(control_data_frame) is False
