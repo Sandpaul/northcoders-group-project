af6c642999ce70af4c888397ea4a87f2c766faec
Sandpaul <paul_sandford@hotmail.co.uk>
2024-03-15 09:26:42 +0000

    refactor fact_payment() to use dataframes rather than json.

--- src/transform/fact_payment.py
+++ src/transform/fact_payment.py
@@ -1,62 +1,21 @@
-"""This module contains the definition for transform_payment()"""
+"""This module contains the definition for `fact_payment()`."""
 
-import pandas as pd
+from src.utils.split_created_and_updated import split_created_and_updated
 
 
-def transform_payment(payment_data):
-    """Function to transform data stored in ingestion bucket that was extracted
-    from payment table in totesys.
+def fact_payment(payment_data):
+    """
+    A function to transform data from payment table in totesys database, ready for insertion into fact_payment table in data warehouse.
 
     Args:
-        payment_data (dict): dict of data from payment file from
-        ingestion bucket
+        payment_data (data frame): a data frame containing data extracted from payments table in totesys.
 
     Returns:
-        payment_data_copy (dict): copy of payment_data with data dict
-        replaced by dataframe
+        df (data frame): the transformed data frame ready for insertion into fact_payment table in data warehouse.
     """
-    payment_data_copy = payment_data.copy()
-    row_data = payment_data_copy["payment"]
 
-    for row in row_data:
-        created_at = row["created_at"].split(" ")
-        row["created_date"] = created_at[0]
-        row["created_time"] = created_at[1]
-        row.pop("created_at")
-        last_updated = row["last_updated"].split(" ")
-        row["last_updated_date"] = last_updated[0]
-        row["last_updated_time"] = last_updated[1]
-        row.pop("last_updated")
-        row.pop("company_ac_number")
-        row.pop("counterparty_ac_number")
-        row["payment_amount"] = float(row["payment_amount"])
+    df = split_created_and_updated(payment_data)
+    df.drop("company_ac_number", axis=1, inplace=True)
+    df.drop("counterparty_ac_number", axis=1, inplace=True)
 
-    payment_data_copy["payment"] = pd.DataFrame.from_records(row_data)
-
-    payment_data_copy["payment"].rename(
-        columns={
-            "transaction_id": "transaction_record_id",
-            "counterparty_id": "counterparty_record_id",
-            "currency_id": "currency_record_id",
-            "payment_type_id": "payment_type_record_id",
-        },
-        inplace=True,
-    )
-
-    payment_data_copy["payment"]["last_updated_date"] = pd.to_datetime(
-        payment_data_copy["payment"]["last_updated_date"], format="%Y-%m-%d"
-    ).dt.date  # noqa
-    payment_data_copy["payment"]["last_updated_time"] = pd.to_datetime(
-        payment_data_copy["payment"]["last_updated_time"], format="mixed"
-    ).dt.time  # noqa
-    payment_data_copy["payment"]["created_date"] = pd.to_datetime(
-        payment_data_copy["payment"]["created_date"], format="%Y-%m-%d"
-    ).dt.date  # noqa
-    payment_data_copy["payment"]["created_time"] = pd.to_datetime(
-        payment_data_copy["payment"]["created_time"], format="mixed"
-    ).dt.time  # noqa
-    payment_data_copy["payment"]["payment_date"] = pd.to_datetime(
-        payment_data_copy["payment"]["payment_date"], format="%Y-%m-%d"
-    ).dt.date  # noqa
-
-    return payment_data_copy
+    return df
--- test/test_transform/test_fact_payment.py
+++ test/test_transform/test_fact_payment.py
@@ -1,156 +1,111 @@
-from src.transform.read_ingestion_file_data import read_ingestion_file_data
-from src.transform.fact_payment import transform_payment
-import os
-import boto3
-from moto import mock_aws
+"""This module contains the test suite for `fact_payment()`."""
+
+import datetime
+import json
+
+import pandas as pd
 import pytest
 
-
-@pytest.fixture(scope="function")
-def aws_credentials():
-    """Mocked AWS Credentials for moto"""
-    os.environ["AWS_ACCESS_KEY_ID"] = "testing"
-    os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
-    os.environ["AWS_SECURITY_TOKEN"] = "testing"
-    os.environ["AWS_SESSION_TOKEN"] = "testing"
-    os.environ["AWS_DEFAULT_REGION"] = "eu-west-2"
-
-
-@pytest.fixture(scope="function")
-def s3(aws_credentials):
-    """Create mock s3 client."""
-    with mock_aws():
-        yield boto3.client("s3", region_name="eu-west-2")
+from src.transform.fact_payment import fact_payment
 
 
 @pytest.fixture
-def example_data():
-    """Create mock data."""
-    return {
-        "timestamp": "2024-02-22 14:50:20.010165",
-        "payment": [
-            {
-                "company_ac_number": 72098432,
-                "counterparty_ac_number": 27214177,
-                "counterparty_id": 4,
-                "created_at": "2024-02-22 14:48:10.434000",
-                "currency_id": 2,
-                "last_updated_time": "2024-02-22 14:48:10.434000",
-                "paid": False,
-                "payment_amount": "82207.80",
-                "payment_date": "2024-02-23",
-                "payment_id": 9881,
-                "payment_type_id": 1,
-                "transaction_id": 9881,
-            },
-            {
-                "company_ac_number": 65525440,
-                "counterparty_ac_number": 19472783,
-                "counterparty_id": 13,
-                "created_at": "2024-02-22 15:59:10.043000",
-                "currency_id": 3,
-                "last_updated_time": "2024-02-22 15:59:10.043000",
-                "paid": False,
-                "payment_amount": "319428.33",
-                "payment_date": "2024-02-26",
-                "payment_id": 9882,
-                "payment_type_id": 1,
-                "transaction_id": 9882,
-            },
-        ],
-    }
-
-
-@pytest.fixture
-def bucket(s3, example_data):
-    """Create mock s3 bucket."""
-    s3.create_bucket(
-        Bucket="totesys-etl-ingestion-bucket-teamness-120224",
-        CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
-    )
+def payment_df():
+    """Sets up a test data frame."""
     with open("test/test_transform/test_data/test_payment_data.json") as f:
-        data_to_write = f.read()
-        s3.put_object(
-            Body=data_to_write,
-            Bucket="totesys-etl-ingestion-bucket-teamness-120224",
-            Key="payment/2024-02-22/14:50:20.010164.json",
-        )
-    with open("test/test_transform/test_data/test_payment_data2.json") as f:
-        data_to_write = f.read()
-        s3.put_object(
-            Body=data_to_write,
-            Bucket="totesys-etl-ingestion-bucket-teamness-120224",
-            Key="payment/2024-02-22/14:50:20.010165.json",
-        )
+        data = f.read()
+        json_data = json.loads(data)
+        df = pd.DataFrame.from_records(json_data["payment"])
+        return df
 
 
-@pytest.mark.describe("transform_payment()")
-@pytest.mark.it("should return dictionary with a dataframe")
-def test_transform_payment_returns_df(s3, bucket, example_data):
-    """should return a dictionary with a dataframe"""
-    file_path = "payment/2024-02-22/14:50:20.010164.json"
-    test_data = read_ingestion_file_data(file_path)
-    result = transform_payment(test_data)
-    assert type(result["payment"]).__name__ == "DataFrame"
+@pytest.fixture
+def control_df():
+    """Sets up a control data frame."""
+    with open("test/test_transform/test_data/test_payment_data.json") as f:
+        data = f.read()
+        json_data = json.loads(data)
+        df = pd.DataFrame.from_records(json_data["payment"])
+        return df
 
 
-@pytest.mark.describe("transform_payment()")
-@pytest.mark.it("dataframe should contain correct number of keys")  # noqa
-def test_dataframe_returns_correct_number_of_keys(s3, bucket, example_data):
-    file_path = "payment/2024-02-22/14:50:20.010164.json"
-    test_data = read_ingestion_file_data(file_path)
-    result = transform_payment(test_data)
-    assert set(result["payment"].keys()) == set(
-        [
-            "payment_record_id",
-            "payment_id",
-            "created_date",
-            "created_time",
-            "last_updated_date",
-            "last_updated_time",
-            "transaction_record_id",
-            "counterparty_record_id",
-            "payment_amount",
-            "currency_record_id",
-            "payment_type_record_id",
-            "paid",
-            "payment_date",
-        ]
-    )
+@pytest.mark.describe("fact_payment()")
+@pytest.mark.it("should return a dataframe")
+def test_fact_payment_returns_df(payment_df):
+    """fact_payment() should return a dataframe"""
+    result = fact_payment(payment_df)
+    assert type(result).__name__ == "DataFrame"
 
 
-@pytest.mark.describe("transform_payment()")
+@pytest.mark.describe("fact_payment()")
+@pytest.mark.it("dataframe should return correct columns")
+def test_dataframe_returns_correct_columns(payment_df):
+    """fact_payment() should return data frame with correcy columns."""
+    result = fact_payment(payment_df)
+    print(list(result.columns))
+    expected = [
+        "counterparty_id",
+        "currency_id",
+        "paid",
+        "payment_amount",
+        "payment_date",
+        "payment_id",
+        "payment_type_id",
+        "transaction_id",
+        "created_date",
+        "created_time",
+        "last_updated_date",
+        "last_updated_time",
+    ]
+    assert list(result.columns) == expected
+
+
+@pytest.mark.describe("fact_payment()")
 @pytest.mark.it(
     "dataframe should contain correct created_date and created_time"
 )  # noqa
-def test_transform_payment_returns_correct_created(s3, bucket, example_data):
+def test_fact_payment_returns_correct_created(payment_df):
     """should return the correct created_date and created_time"""
-    file_path = "payment/2024-02-22/14:50:20.010165.json"
-    test_data = read_ingestion_file_data(file_path)
-    result = transform_payment(test_data)
-    assert result["payment"].get("created_date").get(1) == "2024-02-22"
-    assert result["payment"].get("created_time").get(1) == "15:59:10.043000"
+    result = fact_payment(payment_df)
+    expected_dates = [
+        datetime.date(2024, 2, 22),
+        datetime.date(2024, 2, 22),
+    ]
+    expected_times = [
+        datetime.time(14, 48, 10, 434000),
+        datetime.time(15, 59, 10, 43000),
+    ]
+    dates = result["created_date"].tolist()
+    times = result["created_time"].tolist()
+    assert dates == expected_dates
+    assert times == expected_times
 
 
-@pytest.mark.describe("transform_payment()")
+@pytest.mark.describe("fact_payment()")
 @pytest.mark.it(
     "dataframe should contain correct last_updated_date and last_updated"
 )  # noqa
-def test_transform_payment_returns_correct_last_updated(s3, bucket, example_data):
+def test_fact_payment_returns_correct_last_updated(payment_df):
     """should return the correct last_updated_date and last_updated"""
-    file_path = "payment/2024-02-22/14:50:20.010165.json"
-    test_data = read_ingestion_file_data(file_path)
-    result = transform_payment(test_data)
-    assert result["payment"].get("last_updated_date").get(0) == "2024-02-22"
-    assert result["payment"].get("last_updated_time").get(0) == "14:48:10.434000"
+    result = fact_payment(payment_df)
+    expected_dates = [
+        datetime.date(2024, 2, 22),
+        datetime.date(2024, 2, 22),
+    ]
+    expected_times = [
+        datetime.time(14, 48, 10, 434000),
+        datetime.time(15, 59, 10, 43000),
+    ]
+    dates = result["last_updated_date"].tolist()
+    times = result["last_updated_time"].tolist()
+    assert dates == expected_dates
+    assert times == expected_times
 
 
-@pytest.mark.describe("transform_payment()")
-@pytest.mark.it("should work when passed payment list with more than one dict")  # noqa
-def test_transform_payment_works_on_multiple_dicts(s3, bucket, example_data):
-    file_path = "payment/2024-02-22/14:50:20.010165.json"
-    test_data = read_ingestion_file_data(file_path)
-    result = transform_payment(test_data)
-
-    assert result["payment"].get("transaction_record_id").get(0) == 9881
-    assert result["payment"].get("transaction_record_id").get(1) == 9882
+@pytest.mark.describe("fact_payment()")
+@pytest.mark.it("should not mutate passed data frame")
+def test_does_not_mutate_arg(payment_df, control_df):
+    """fact_payment() should not mutate the passed data frame."""
+    result = fact_payment(payment_df)
+    assert payment_df.equals(control_df) is True
+    assert result.equals(payment_df) is False
